252aca4a22c09f887f12415fd741ba908fc26c03
Abdallahi Nah <abdallahi.nah.un.fst@gmail.com>
2025-10-10 08:47:25 +0000

    commit

---
+++ cpp-course-8/prob12.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct sDate {
+    int day;
+    int month;
+    int year;
+};
+
+int readDay() {
+    int day;
+    do {
+        cout << "Please enter a day between 1 and 31 : " << endl;
+        cin >> day;
+    }while(day < 1 || day > 31);
+    return day;
+}
+
+int readMonth() {
+    int month;
+    do {
+        cout << "Please enter a month between 1 and 12: ";
+        cin >> month;
+    } while (month < 1 || month > 12);
+    return month;
+}
+
+int readYear() {
+    int year;
+    cout << "Please enter a year: ";
+    cin >> year;
+    return year;
+}
+
+sDate readDate() {
+    sDate date;
+    date.day = readDay();
+    date.month = readMonth();
+    date.year = readYear();
+    return date;
+}
+
+bool isLeapYearOptimized(int year) {
+    return ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)));
+}
+
+int numberOfDaysInMonth(int year, int month) {
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            return 31;
+        case 4: case 6: case 9: case 11:
+            return 30;
+        case 2:
+            return isLeapYearOptimized(year) ? 29 : 28;
+        default:
+            return 0;
+    }
+}
+
+bool isLastDayInAMonth(sDate date) {
+    return (date.day == numberOfDaysInMonth(date.year, date.month));
+}
+
+bool isLastMonthInAYear(int month) {
+    return (month == 12);
+}
+
+sDate dateAfterAddingOneDay(sDate date) {
+    if(isLastDayInAMonth(date)) {
+        date.day = 1;
+        if(isLastMonthInAYear(date.month)) {
+            date.month = 1;
+            date.year++;
+        } else {
+            date.month++;
+        }
+    } else {
+        date.day++;
+    }
+
+    
+
+    return date;
+}
+
+int main() {    
+    sDate date = readDate();
+    date = dateAfterAddingOneDay(date);
+
+    cout << "\nDate after adding one day is : " << 
+             date.day << "/" << date.month << 
+             "/" << date.year << endl;
+
+    return 0;
+}
