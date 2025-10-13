7ea414dfc8d9a369d0413ce05eb2e58a80d582b1
Abdallahi Nah <abdallahi.nah.un.fst@gmail.com>
2025-10-13 09:23:10 +0000

    commit

--- cpp-course-8/prob22.cpp
+++ cpp-course-8/prob22.cpp
@@ -45,6 +45,51 @@ sDate readDate() {
     return date;
 }
 
+bool isDate1AfterDate2(sDate date1, sDate date2) {
+    return (date1.year > date2.year ? true : (date1.month > date2.month ? true : (date1.day > date2.day ? true : false)));
+}
+
+bool isDate1EqualDate2(sDate date1, sDate date2) {
+    return (date1.year == date2.year && date1.month == date2.month && date1.day == date2.day);
+}
+
+bool isDate1BeforeDate2(sDate date1, sDate date2) {
+    return (!isDate1AfterDate2(date1, date2) && !isDate1EqualDate2(date1, date2));
+}
+
+short compareDates(sDate date1, sDate date2) {
+    if(isDate1BeforeDate2(date1, date2))
+        return -1;
+    else if(isDate1EqualDate2(date1, date2))
+        return 0;
+    else
+        return 1;
+}
+
+
+bool isOverlapDates(sPeriod period1, sPeriod period2) {
+    if((compareDates(period2.date2, period1.date1) == -1) 
+        || (compareDates(period1.date2, period2.date1) == -1)) {
+        return false;
+    } else {
+        return true;
+    }
+
+    // if((period1.date2.year < period2.date1.year) || (period2.date2.year < period1.date1.year)) {
+    //     return false;
+    // } else {
+    //     if(((period1.date2.year == period2.date1.year) && (period1.date2.month < period2.date1.month)) || ((period2.date2.year == period1.date1.year) && (period2.date2.month < period1.date1.month))) {
+    //         return false;
+    //     } else {
+    //         if(((period1.date2.year == period2.date1.year) && (period1.date2.month == period2.date1.month) && (period1.date2.day < period2.date1.day)) || ((period2.date2.year == period1.date1.year) && (period2.date2.month == period1.date1.month) && (period2.date2.day < period1.date1.day))) {
+    //             return false;
+    //         } else {
+    //             return true;
+    //         }
+    //     }
+    // }
+}
+
 int main() {
     sPeriod period1, period2;
     sDate date1, date2, date3, date4;
@@ -65,7 +110,11 @@ int main() {
     period2.date1 = date3;
     period2.date2 = date4;
 
-    
+    if(isOverlapDates(period1, period2)) {
+        cout << "\nYes, Periods Overlap" << endl;
+    } else {
+        cout << "\nNo, Periods NOT Overlap" << endl;
+    }
 
     return 0;
 }
